147feebb3821f34306a505736237b892971e8fc5
Gabriela Gutierrez <dcarp2001@hotmail.com>
2024-05-29 20:49:55 -0300

    playing with the linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,11 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+module.exports = [
+  {
+    rules: {
+      eqeqeq: "error", // Requiere el uso de === y !==
+      curly: "error", // Requiere llaves después de las declaraciones condicionales
+      "prefer-const": "warn",
+      "no-debugger": "error",
+      "no-var": "warn",
+    },
   },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {
-    "no-eval": "error", // Prohíbe el uso de eval()
-    "no-alert": "warn", // Advertencia para el uso de alert()
-    eqeqeq: "error", // Requiere el uso de === y !==
-    curly: "error", // Requiere llaves después de las declaraciones condicionales
-    "no-undef": "error", // Prohíbe el uso de variables indefinidas
-    "no-unused-vars": "warn", // Advertencia para variables no utilizadas
-    "no-console": "warn", // Advertencia para console.log
-  },
-};
+];
--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ afterAll(() => {
 
 describe("Simple Test", () => {
   it("should return Hello World!", async () => {
-    const response = await request(app).get("/");
+    var response = await request(app).get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Hello World!");
   });
